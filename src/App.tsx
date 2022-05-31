dfac9a3ac5b66c1b24e97a07e49605bedf1fff44
erikryan-s <erik1998serrano@outlook.com>
2022-05-31 11:04:55 +1000

    converted to styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import "./App.scss";
-import Counter from "./components/Counter/Counter";
+import Counter from "./components/Counter";
 
 const App = () => {
     return (
         <div className="app">
-            <h1>Scoreboard</h1>
+            <h1>🏐Scoreboard🏐</h1>
             <div className="scoreboard">
                 <div className="teamOne">
-                    <Counter title="Home" initialCount={0} />
+                    <Counter title="Home" initialCount={0} isTeamOne={true} />
                 </div>
                 <div className="teamTwo">
-                    <Counter title="Guest" initialCount={0} />
+                    <Counter title="Guest" initialCount={0} isTeamOne={false} />
                 </div>
             </div>
         </div>
